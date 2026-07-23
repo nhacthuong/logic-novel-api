e5acdc0ea58cb3b6ed90efebbd5bdf7372b168e4
nhacthuong <nhacthuong.v3@gmail.com>
2026-07-23 19:40:04 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,9 +1,9 @@
 name = "logic-novel-api"
 main = "worker.js"
-compatibility_date = "2024-03-01"
+compatibility_date = "2026-07-23"
 
 # Cấu hình kết nối D1 Database
 [[d1_databases]]
 binding = "DB"
 database_name = "logic_novel_db" # Tên database D1 của bạn
-database_id = "dán-id-d1-database-của-bạn-vào-đây"
+database_id = "f065b776-1b54-4327-8054-d723929894b1"
