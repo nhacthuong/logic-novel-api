a2024b5bbc449b15b6ca1afb3fe0e3d603763291
nhacthuong <nhacthuong.v3@gmail.com>
2026-07-23 19:38:45 +0700

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,9 @@
+name = "logic-novel-api"
+main = "worker.js"
+compatibility_date = "2024-03-01"
+
+# Cấu hình kết nối D1 Database
+[[d1_databases]]
+binding = "DB"
+database_name = "logic_novel_db" # Tên database D1 của bạn
+database_id = "dán-id-d1-database-của-bạn-vào-đây"
